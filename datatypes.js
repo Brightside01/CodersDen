38c5f5f072114432ce70b6f94fa393040e2f5e41
Bright <j.brovig@outlook.com>
2022-04-10 12:25:02 +0100

    first commit

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+let firstname = 'Bright'
+let male = true
+let relationship;
+let kids = null
+
+console.log(typeof 'Bright')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof relationship)
---
+++ variable.js
@@ -0,0 +1,22 @@
+let me = "Brightside"
+let us = 1
+let we = true
+let is = null
+
+let mE;
+let Us;
+let w_e;
+let is_;
+
+let firstName = 'Chukwudi', LastName = 'Nwabueze', Nationality = 'Country',  age = 29
+
+let Name = 'Chukwudi'
+let Lastname = 'Nwabueze'
+let nationality = 'Country'
+let aGe = 29
+
+let myAge = ['I', 'am', 25, 'years', 'old']
+let yourAge = ['You', 'are', 30, 'years', 'old']
+
+console.log(myAge)
+console.log(yourAge)
